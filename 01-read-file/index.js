70044284bb1211b40ce876309f46b3bd6510d1b8
Andrey Zharkikh <orbital.logistics@yandex.ru>
2025-01-16 15:45:37 +0300

    feat: task 01-read-file is complete

---
+++ 01-read-file/index.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const fs = require('fs');
+
+const filePath = path.join(__dirname, 'text.txt');
+const readStream = fs.createReadStream(filePath, 'utf-8');
+let result = '';
+
+readStream.on('data', (chunk) => {
+  result += chunk;
+});
+readStream.on('error', (err) => {
+  console.log(err);
+});
+readStream.on('end', () => {
+  console.log(result);
+});
